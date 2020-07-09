9befdd35d92c32618cb3f4d6a486be5516bcbd63
Harley Hanna <harleyhannawrit@gmail.com>
2020-07-09 20:24:44 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -302,7 +302,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     input = gets.chomp
-    input.to_s
+    input.to_i
     puts "Playing #{list_songs[input]}"
   end
   
